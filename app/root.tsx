1e60a373f9be32a723106fa87af50b95c1528977
wan2000 <wansj@samsundot.com>
2024-05-12 20:21:19 +0800

    init

---
+++ app/root.tsx
@@ -0,0 +1,44 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import { getMuiLinks } from "./mui/getMuiLinks";
+import { MuiMeta } from "./mui/MuiMeta";
+import { LinksFunction } from "@remix-run/node";
+import { MuiDocument } from "./mui/MuiDocument";
+
+export const links: LinksFunction = () => [...getMuiLinks()];
+
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        {/* <MuiMeta /> */}
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <MuiDocument>
+        <Outlet />
+      </MuiDocument>
+    </>
+  );
+}
---
+++ app/routes/_index.tsx
@@ -0,0 +1,44 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+import {Button} from "@mui/material";
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix</h1>
+      <Button>Button</Button>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+        <Link to="/mui">Go to Mui Example</Link>
+      </ul>
+    </div>
+  );
+}
